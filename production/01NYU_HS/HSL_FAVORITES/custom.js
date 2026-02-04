3d89faaa791d6416b2a3006ac63ff10257810313
Michael Haag <mh92@nyu.edu>
2026-02-04 15:40:04 -0500

    202602041533

--- production/01NYU_HS/HSL_FAVORITES/custom.js
+++ production/01NYU_HS/HSL_FAVORITES/custom.js
@@ -56,4 +56,44 @@ var app = angular.module('viewCustom', ['angularLoad']);
   });
 // ... End BrowZine - Primo Integration
 
+
+  /**
+   * Customize user Favorites page, Jan 2026
+   */
+  app.component('prmFavoritesToolBarAfter', {
+    bindings: { parentCtrl: '<' },
+    template: `<div ng-if="!$ctrl.isSignedIn()" layout="row" layout-align="center center">
+                  <md-card flex="80">
+                  <md-card-title>
+                      <md-card-title-text layout-align="center center"><span class="md-headline">
+                      Please <strong><a href="https://search.hsl.med.nyu.edu/discovery/login?vid=01NYU_HS:HSL_FAVORITES">log in</a></strong> to view your favorite resources!</span>
+          
+                      </md-card-title-text>
+                      <md-card-title-media>
+                      <div class="md-media-sm card-media"></div>
+                      </md-card-title-media>
+                  </md-card-title>
+                  </md-card>
+              </div>
+              <div ng-if="$ctrl.isSignedIn() && !$ctrl.resultsExists" layout="row" layout-align="center center">
+                         <md-card flex="80">
+                         <md-card-title>
+                             <md-card-title-text layout-align="center center"><span class="md-headline">
+                             To add to your favorites list, search for your favorite resources and look for the Pin icon: <svg id="prm_pin_cache229" width="24" height="24" viewBox="0 0 24 24" y="0" xmlns="http://www.w3.org/2000/svg" fit="" preserveAspectRatio="xMidYMid meet" focusable="false">
+        <path d="M16,12V4H17V2H7V4H8V12L6,14V16H11.2V22H12.8V16H18V14L16,12Z"></path>
+    </svg>
+                            <br/><br/>
+                            Any pinned items will automatically appear here, and on the HSL homepage.</span>
+                             </md-card-title-text>
+                             <md-card-title-media>
+                             <div class="md-media-sm card-media"></div>
+                             </md-card-title-media>
+                         </md-card-title>
+                         </md-card>
+                     </div>
+                     `
+  });
+
+  
+
 })();
